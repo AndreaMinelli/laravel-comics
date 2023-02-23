ac48517854bbadbedfe8102ea2d16f78b21bb0da
AndreaMinelli <minelliandrea94@gmail.com>
2023-02-23 17:15:27 +0100

    Creata section current-series in comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <h1>Comics</h1>
+    <section id="current-series">
+        <div class="container">
+            <h2>CURRENT SERIES</h2>
+            <ul class="row">
+                @foreach ($comics as $comic)
+                    <li class="comic-card">
+                        <figure>
+                            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}" />
+                        </figure>
+                        <p>{{ $comic['series'] }}</p>
+                    </li>
+                @endforeach
+            </ul>
+            <div class="load-comics">
+                <button>LOAD MORE</button>
+            </div>
+        </div>
+    </section>
 @endsection
